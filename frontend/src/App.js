de1327f629aca1b6707ce997b2facd528bd42f7e
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2024-02-29 03:48:55 +0530

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
-  const [auth] = useAuth();
   return (
     <Router>
       <div className="container-fluid">
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
+      
 
         <Routes>
          
