087eb5604b925f270af549a9ca0a96708c522a2d
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2024-02-29 03:45:24 +0530

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Tweet from './Pages/Tweet';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import SignUp from './Pages/SignUp';
-import UserProfile from './Pages/UserProfile';
-import { ToastContainer } from 'react-toastify';
-import OtherProfile from './Pages/OtherProfile';
-import { useAuth } from './context/auth';
-import BackToLoginSpinner from './components/spinner/BackToLoginSpinner';
 
 
 function App() {
@@ -31,19 +22,11 @@ function App() {
         />
 
         <Routes>
-          {auth?.token ? (<>
-            <Route path='/' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/myprofile/:id' element={<UserProfile />} />
-            <Route path='/profile/:id' element={<OtherProfile />} />
-            <Route path='/detail/:id' element={<Tweet />} />
-
-          </>) : (<>
-            <Route path='/' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='*' element={<BackToLoginSpinner />} />
-          </>)}
+         
+            <Route path='/' element={<>
+            hiii
+            </>} />
+           
 
         </Routes>
 
